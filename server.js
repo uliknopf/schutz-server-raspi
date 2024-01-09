845d20aa979cb25978a33c0835608a9972c7af1f
uli <uliknopf@web.de>
2024-01-09 09:52:39 +0100

    Korrektur

--- server.js
+++ server.js
@@ -13,20 +13,20 @@ const fs=require('fs');
 const ipdatei= './blacklist.txt';
 
 //prüft ob Datei schwarzeListe existiert und legt sie bei Bedarf neu an
-fs.stat(ipdatei, (err, stat) => { 
-  if (err) {
-    fs.open(ipdatei, 'w', function (err, file) {
-      if (err) throw err;
-    }); 
-  }
+fs.stat(ipdatei, (err, stat) => {
+	if (err) {
+		fs.open(ipdatei, 'w', function (err, file) {
+			if (err) throw err;
+		}); 
+	}
 });
 //Funktion:  IP-Adressen, die ignoriert werden sollen, in Datei schreiben
 async function appendToFile(data) { 
-  try {
-     await fs.promises.appendFile(ipdatei, data, { flag: 'a' });
-    } catch (error) {
-    console.error(`Fehlermeldung: {error.message}`);
-  }
+	try {
+		await fs.promises.appendFile(ipdatei, data, { flag: 'a' });
+	} catch (error) {
+		console.error(`Fehlermeldung: {error.message}`);
+	}
 }
 //Daten, werden von node-red angefordert ,
 // oder hier die eigenen Daten bereitstellen
@@ -47,40 +47,39 @@ const ipBanList = {};
 const timeFrame = 1000; // 1 Sekunde
 
 const server = http.createServer((req, res) => {
-  // IP-Adresse ermitteln
-  const ipAddress = req.socket.remoteAddress;
-  // Zeitmessung beginnen
-  const zeit = new Date();
-  const now = zeit.getTime();
-  // Checke API-Methode und URL
-  if (req.method === 'GET' && req.url === '/meinURL' ) {
-    // Checke, ob IP-Adresse schon als "böse" gemeldet ist
-	  // oder ob mehr als einmal pro Sekunde ein Request gestellt wurde
-    if (ipZeitinfo[ipAddress] && ((now - ipZeitinfo[ipAddress] < timeFrame)||ipBanList[ipAddress])) {
-      //setze IP-Banned-Liste auf true
-      ipBanList[ipAddress] = true;
-      // Die Antwort des Servers an den "bösen" - Daten liefern
-      res.statusCode = 403;// Verboten
-      res.end('NoNo');
-      } else {
-		// Update Zeitmessung des letzten Requests
-		ipZeitinfo[ipAddress] = now;
-		//Die Antwort des Servers an den "guten" - Daten liefern
-		fetchy().then(databaseAnswer=>{
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.end(databaseAnswer);
-        });
-      }
-    }
-	else if (req.method === 'GET' && req.url === '/unsagbarSchwereURL'){//Abbruchbedingung des Servers
-    server.close();
-	  res.statusCode = 200;// beenden
-    res.end('ByeBye');
+	// IP-Adresse ermitteln
+	const ipAddress = req.socket.remoteAddress;
+	// Zeitmessung beginnen
+	const zeit = new Date();
+	const now = zeit.getTime();
+	// Checke API-Methode und URL
+	if (req.method === 'GET' && req.url === '/meinURL' ) {
+		// Checke, ob IP-Adresse schon als "böse" gemeldet ist
+		// oder ob mehr als einmal pro Sekunde ein Request gestellt wurde
+		if (ipZeitinfo[ipAddress] && ((now - ipZeitinfo[ipAddress] < timeFrame)||ipBanList[ipAddress])) {
+			//setze IP-Banned-Liste auf true
+			ipBanList[ipAddress] = true;
+			// Die Antwort des Servers an den "bösen" - Daten liefern
+			res.statusCode = 403;// Verboten
+			res.end('NoNo');
+		} else {
+			// Update Zeitmessung des letzten Requests
+			ipZeitinfo[ipAddress] = now;
+			//Die Antwort des Servers an den "guten" - Daten liefern
+			fetchy().then(databaseAnswer=>{
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.end(databaseAnswer);
+			});
+		}
+	} else if (req.method === 'GET' && req.url === '/unsagbarSchwereURL'){ //Abbruchbedingung des Servers
+		server.close();
+		res.statusCode = 200; // beenden
+		res.end('ByeBye');
 	}else { // alle anderen Anfragen sind falsch und zu ignorieren
-    ipBanList[ipAddress] = true;
+		ipBanList[ipAddress] = true;
 		// ZeitStempel erstellen
-    const zeitString=zeit.toString();
-		// text erstellen, der in die Datei SchwarzeListe geschrieben wird
+		const zeitString=zeit.toString();
+		// Text erstellen, der in die Datei SchwarzeListe geschrieben wird
 		const listContent = ipAddress+", "+zeitString+", "+req.method+", "+req.url+"\n";
 		// in die Datei schreiben
 		appendToFile(listContent);
@@ -90,7 +89,7 @@ const server = http.createServer((req, res) => {
 		if (stat.size>10000){server.close();} //Abbruchbedingung 
 		res.writeHead(403,{'Content-Type':'text/plain'});
 		res.end('NoNo');
-    }
+	}
 });
 const port = 9000;
 server.listen(port, () => {
